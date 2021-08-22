9448407c5981a362651037fb866b31c157cd9b72
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2021-08-22 07:50:44 -0500

    added one slow test to evaluate fast tests

--- tests/integration/alignment/test_alignment_summary_integration.py
+++ tests/integration/alignment/test_alignment_summary_integration.py
@@ -12,7 +12,7 @@ here = Path(__file__)
 
 @pytest.mark.integration
 class TestAlignmentSummary(object):
-    @patch("builtins.print")
+    @pytest.mark.slow
     def test_alignment_summary_invalid_input(self, mocked_print):  # noqa
         with pytest.raises(SystemExit) as pytest_wrapped_e:
             Biokit()
