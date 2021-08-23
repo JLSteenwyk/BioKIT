d25904d04d4b7081f0c5e88827d1b87f762bab44
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2021-08-23 15:08:32 -0500

    wrote percent argument testing for subseting pe reads

--- tests/integration/fastq/test_subset_pe_fastq_reads.py
+++ tests/integration/fastq/test_subset_pe_fastq_reads.py
@@ -60,6 +60,47 @@ class TestSubsetPEFastQReads(object):
         assert expected_fq_2 == output_fq_2
 
     @patch("builtins.print")
+    def test_subset_pe_fastq_reads_percent(self, mocked_print):
+        input_file_1 = (
+            f"{here.parent.parent.parent}/sample_files/DRR284700_1_subset.fastq"
+        )
+        input_file_2 = (
+            f"{here.parent.parent.parent}/sample_files/DRR284700_2_subset.fastq"
+        )
+
+        testargs = [
+            "biokit",
+            "subset_pe_fastq_reads",
+            input_file_1,
+            input_file_2,
+            "-s",
+            "154",
+            "-p",
+            "80",
+        ]
+        with patch.object(sys, "argv", testargs):
+            Biokit()
+
+        with open(
+            f"{here.parent.parent}/expected/DRR284700_1_subset_percent80.fq", "r"
+        ) as expected_fq_1, open(
+            f"{here.parent.parent}/expected/DRR284700_2_subset_percent80.fq", "r"
+        ) as expected_fq_2:
+            expected_fq_1 = expected_fq_1.read()
+            expected_fq_2 = expected_fq_2.read()
+
+        output_file_1 = re.sub(".fastq$|.fq$", "_subset.fq", input_file_1)
+        output_file_2 = re.sub(".fastq$|.fq$", "_subset.fq", input_file_2)
+        with open(output_file_1, "r") as output_fq_1, open(
+            output_file_2, "r"
+        ) as output_fq_2:
+            output_fq_1 = output_fq_1.read()
+            output_fq_2 = output_fq_2.read()
+
+        assert expected_fq_1 == output_fq_1
+        assert expected_fq_2 == output_fq_2
+
+    @patch("builtins.print")
     def test_subset_pe_fastq_reads_alias(self, mocked_print):
         input_file_1 = (
             f"{here.parent.parent.parent}/sample_files/DRR284700_1_subset.fastq"
