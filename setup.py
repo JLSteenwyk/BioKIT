1d0f523dd359da61a298ee39b16e1dc2e2881693
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2021-08-23 13:14:52 -0500

    testing removing cython as a requirement

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ CLASSIFIERS = [
     "Topic :: Scientific/Engineering",
 ]
 
-REQUIRES = ["biopython==1.76", "numpy==1.18.2", "cython"]
+REQUIRES = ["biopython==1.76", "numpy==1.18.2"]
 
 setup(
     name="biokit",
