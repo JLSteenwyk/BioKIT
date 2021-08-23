ea77265cac6833ce9e0e0a79d117d257865fe050
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2021-08-22 21:17:10 -0500

    added verbose tests for gc at codon position calculations

--- tests/integration/coding_sequences/test_gc_content_third_position.py
+++ tests/integration/coding_sequences/test_gc_content_third_position.py
@@ -3,6 +3,7 @@ import pytest
 from mock import patch, call
 from pathlib import Path
 import sys
+import textwrap
 
 from biokit.biokit import Biokit
 
@@ -32,3 +33,29 @@ class TestAlignmentSummary(object):
         with patch.object(sys, "argv", testargs):
             Biokit()
         assert mocked_print.mock_calls == [call(expected_result)]
+
+    @patch("builtins.print")
+    def test_gc_content_third_position_simple_verbose(self, mocked_print):
+        expected_result_0 = textwrap.dedent(
+            """lcl|NC_001133.9_cds_NP_009332.1_1\t0.4876"""
+        )
+        expected_result_1 = textwrap.dedent(
+            """lcl|NC_001133.9_cds_NP_878038.1_2\t0.4211"""
+        )
+        expected_result_2 = textwrap.dedent(
+            """lcl|NC_001133.9_cds_NP_009333.1_3\t0.3956"""
+        )
+
+        testargs = [
+            "biokit",
+            "gc_content_first_position",
+            f"{here.parent.parent.parent}/sample_files/GCF_000146045.2_R64_cds_from_genomic.small.fna",
+            '-v',
+        ]
+        with patch.object(sys, "argv", testargs):
+            Biokit()
+        assert mocked_print.mock_calls == [
+            call(expected_result_0),
+            call(expected_result_1),
+            call(expected_result_2),
+        ]
